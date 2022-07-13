b8c4aa0a8792b6a85e9c21e06be422e55af538d1
jannik.loeber <jloeber@rhrk.uni-kl.de>
2022-07-13 12:57:42 +0200

    Adding frontend layout for creating a watering job

---
+++ farmbot-app/src/components/CreateWateringJob.svelte
@@ -0,0 +1,56 @@
+<script>
+    import ActionItem from "./ActionItem.svelte";
+
+
+
+    let plantType = "radish";
+    let amountOfWater = 100;
+    let height = 0;
+
+
+
+    function create() {
+        //TODO: create watering job, send data to endpoint
+    }
+
+
+
+
+</script>
+
+<ActionItem description="Create a new watering job">
+    <div>
+        <table id="myTable" border="0" cellpadding="3">
+            <tr>
+                <td>Plant type:</td>
+                <td><input bind:value={plantType}></td>
+            </tr>
+            <tr>
+                <td>Amount of water per plant (in ml):</td>
+                <td><input type = "number" bind:value={amountOfWater}></td>
+            </tr>
+            <tr>
+                <td>Height (in mm):</td>
+                <td><input type = "number" bind:value={height}></td>
+            </tr>
+        </table>
+        <button on:click={create}>
+            Create job
+        </button>
+
+    </div>
+</ActionItem>
+
+<style>
+    main {
+        text-align: center;
+        padding: 16px;
+        margin: auto;
+    }
+
+    #myTable {
+        table-layout: fixed;
+    }
+
+
+</style>
--- farmbot-app/src/components/JobControls.svelte
+++ farmbot-app/src/components/JobControls.svelte
@@ -2,11 +2,13 @@
   import CreateSeedingJob from "./CreateSeedingJob.svelte";
   import BotStatus from "./BotStatus.svelte";
   import ExecuteJob from "./ExecuteJob.svelte";
+  import CreateWateringJob from "./CreateWateringJob.svelte";
 </script>
 
 <div class="container">
   <CreateSeedingJob />
   <ExecuteJob />
+  <CreateWateringJob />
   <BotStatus />
 </div>
 
